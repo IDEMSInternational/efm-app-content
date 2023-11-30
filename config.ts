ff23720103951519e39b58dc4fe23195aa2fbd89
cabrinenyona <89448221+cabrinenyona@users.noreply.github.com>
2023-11-30 14:03:21 +0300

    content: 1.1.5

--- config.ts
+++ config.ts
@@ -13,7 +13,7 @@ config.translations = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/efm-app-content.git",
-  content_tag_latest: "1.1.4",
+  content_tag_latest: "1.1.5",
 };
 
 config.api.db_name = "early_family_math"
