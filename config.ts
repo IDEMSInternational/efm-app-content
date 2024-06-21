21417a98deff836ab8af26aa64297b60c2118037
Chris Wright <chris@kitchentablemath.com>
2024-06-21 16:17:12 -0700

    content: 1.2.16

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/efm-app-content.git",
-  content_tag_latest: "1.2.15",
+  content_tag_latest: "1.2.16",
 };
 
 config.android = {
