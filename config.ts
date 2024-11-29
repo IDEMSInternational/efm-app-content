5f9349c1ed8fd6ac50eb28b93acf7e74b7c3fa86
EsmeeIDEMS <esmee@idems.international>
2024-11-29 12:24:42 +0000

    content: 1.2.33

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/efm-app-content.git",
-  content_tag_latest: "1.2.32",
+  content_tag_latest: "1.2.33",
 };
 
 config.android = {
