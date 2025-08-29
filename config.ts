a5319de5c10dec6976e03c75298b176f082a7721
GitHub Action <action@github.com>
2025-08-29 14:29:36 +0000

    Automated commit message

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/efm-app-content.git",
-  content_tag_latest: "1.2.41",
+  content_tag_latest: "1.2.42",
 };
 
 config.android = {
